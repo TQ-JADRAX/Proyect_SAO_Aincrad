7cf6c786fd5913db3c346207edfb28d01a1c1e3b
ALEXYS AVENDANO MONFORT <alexysavenda110406@gmail.com>
2025-06-28 09:37:37 -0600

    Remake entorno virtual .venv

--- API-AI_Commits/.venv/Scripts/activate.bat
+++ API-AI_Commits/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Proyect_SAO_Aincrad\API-Gpt\.venv
+set VIRTUAL_ENV=C:\Proyect_SAO_Aincrad\API-AI_Commits\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
